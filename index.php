0a9fc0603143a70a3abdc5c56f897165e7c3dff3
sorshireality <tutunikssa@gmail.com>
2022-01-30 12:42:50 +0200

    add log

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ require_once "generator.php";
 
 $request_array = array_values(array_filter(explode('/', $_SERVER['REQUEST_URI'])));
 
+$file = "log.txt";
+$current = file_get_contents($file);
+$message['request'] = $request_array;
+
 $response = "";
 if (count($request_array) == 1 && $request_array[0] == 'user') {
     header("HTTP/1.1 200 OK");
@@ -23,4 +27,7 @@ function NotFound(&$response)
 }
 
 header('Content-Type: application/json');
+$message['response'] = $response;
+$current .= json_encode($message);
+file_put_contents($file, $current);
 echo json_encode($response);
